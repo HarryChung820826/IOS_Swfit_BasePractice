fc4dea390eb2e4a892b0b6129f48df974132321e
HarryChung <820826harrychung@gmail.com>
2017-10-31 22:49:34 +0800

    viewDidAppear show alert and use button click show alert example

--- BaseElementPractice/ViewController.swift
+++ BaseElementPractice/ViewController.swift
@@ -20,6 +20,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        showDialog()
+        
+    }
+    @IBAction func BtnClick(_ sender: Any) {
+        showDialog()
+    }
+    
+    private func showDialog(){
+        //have .alert and .actionsheet two style
+        let alertController = UIAlertController(
+            title: "AlertTitle",
+            message: "AlertMessage",
+            preferredStyle: .alert)
+        
+        //Declare a button
+        let okAction = UIAlertAction(
+            title: "ok",
+            style: .default)
+            //this block section is click listener
+        {
+            (action) in
+            self.dismiss(animated: true, completion: nil)
+        }
+        
+        alertController.addAction(okAction)
+        //show alert , like android's alert.show();
+        show(alertController, sender: self)
+    }
 }
 
