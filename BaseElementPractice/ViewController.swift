00f7cd8c4e95930b8afa68d1b97bc635ea8ea03e
HarryChung <820826harrychung@gmail.com>
2017-11-01 22:26:53 +0800

    利用訊息框輸入資料; UIAlertController.preferredStyle : preferredStyle —> bottomSheet 效果

--- BaseElementPractice/ViewController.swift
+++ BaseElementPractice/ViewController.swift
@@ -22,19 +22,27 @@ class ViewController: UIViewController {
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        //UIAlertController 必須等到 View Controller 載入後 才能顯示 ; 因此在viewDidAppear 中處理
         showDialog()
         
     }
+    var lock:Bool = true
     @IBAction func BtnClick(_ sender: Any) {
-        showDialog()
+        if(lock){
+            showDialog()
+        }else{
+            showTextFieldDialog()
+        }
+        lock = !lock
     }
     
+    //顯示一個 訊息框
     private func showDialog(){
         //have .alert and .actionsheet two style
         let alertController = UIAlertController(
             title: "AlertTitle",
             message: "AlertMessage",
-            preferredStyle: .alert)
+            preferredStyle: .actionSheet)
         
         //Declare a button
         let okAction = UIAlertAction(
@@ -49,6 +57,50 @@ class ViewController: UIViewController {
         alertController.addAction(okAction)
         //show alert , like android's alert.show();
         show(alertController, sender: self)
+    }
+    
+    //利用訊息框輸入資料
+    private func showTextFieldDialog(){
+        let alert = UIAlertController(
+            title: "Login Message",
+            message: "Input Account and Password",
+            preferredStyle: .alert)
+        //取消按鈕
+        let cancelAction = UIAlertAction(
+            title: "Cancel"
+            , style: .cancel)
+        {
+            (action) in self.dismiss(animated: true, completion: nil)
+        }
+        //登入按鈕
+        let loginAction = UIAlertAction(
+            title:"Login"
+        ,style:.default)
+        {
+            (action) in
+            let uid = alert.textFields![0].text
+            let pwd = alert.textFields![1].text
+            
+            print("Account :  \(uid)")
+            print("Password :  \(pwd)")
+        }
+        
+        //產生第一個文字輸入框
+        alert.addTextField{
+            (textField) in
+            textField.placeholder = "Login"
+        }
+
+        alert.addTextField{
+            (textField) in
+            textField.placeholder = "Password"
+            //密碼型態
+            textField.isSecureTextEntry =  true
+        }
+        
+        alert.addAction(cancelAction)
+        alert.addAction(loginAction)
+        show(alert, sender: self)
     }
 }
 
